2493ea214bfc82c07001388432d0e95311e38dcd
Megha Pavankumar Panchani <W0836064@myscc.ca>
2023-10-31 10:44:47 -0400

    Hello

--- MeghaBookStore/Startup.cs
+++ MeghaBookStore/Startup.cs
@@ -23,6 +23,7 @@ namespace MeghaBookStore
         }
 
         public IConfiguration Configuration { get; }
+        //megha
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
