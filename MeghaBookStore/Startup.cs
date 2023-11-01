46e1355023c14d9d9c2fa0fab7296f523897d07d
W0836064 <W0836064@ADL-A1201I-12>
2023-11-01 18:51:09 -0400

    IUnitOfWork completed

--- MeghaBookStore/Startup.cs
+++ MeghaBookStore/Startup.cs
@@ -1,17 +1,13 @@
+using MeghaBooks.DataAccess.Repository;
 using MeghaBookStore.DataAccess.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IUnitOfWork = MeghaBooks.DataAccess.Repository.IRepository.IUnitOfWork;
 
 namespace MeghaBookStore
 {
@@ -35,6 +31,7 @@ namespace MeghaBookStore
 
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllersWithViews();
         }
 
---
+++ MeghaBooks.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using MeghaBooks.DataAccess.Repository.IRepository;
+using MeghaBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeghaBooks.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork  // make the method public to access the class
+    {
+        private readonly ApplicationDbContext _db;  // the using statement 
+
+        public UnitOfWork(ApplicationDbContext db)  // constructor  to use DI and inject to the repository
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public ISP_Call SP_Call { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()  //all chnages will be saved when the Save method is call at the 'parent' level
+        {
+            _db.SaveChanges();
+        }
+    }
+}
