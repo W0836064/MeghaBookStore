06d2c2c02d6ec9312d983bc62c1735fb01ee8f36
W0836064 <W0836064@ADL-A1201I-12>
2023-11-01 17:55:53 -0400

    Category completed

---
+++ MeghaBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using MeghaBooks.DataAccess.Repository.IRepository;
+using MeghaBooks.Models;
+using MeghaBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeghaBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+           
+             // use .Net LINQ to retrieve the first or default category object.
+            //then pass the id as a generic entity which matches the category ID
+
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)  //save the changes if not null
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+
+
+        }
+    }
+}
